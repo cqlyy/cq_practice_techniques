cb369f0bd2752705f4924470958c0ea703d6cbd1
zhuhai <13550994551@163.com>
2020-04-15 18:39:50 +0800

    feat:新增提供者

---
+++ cq_techniques_common/src/main/java/cq/techniques/CommonApplication.java
@@ -0,0 +1,16 @@
+package cq.techniques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class CommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class,args);
+    }
+}
